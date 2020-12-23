ea8ecf1c8e7361d2abaa82aa3984aaa7f5f326de
Kostis Andriopoulos <kandrio@arrikto.com>
2020-12-23 15:58:41 +0200

    Remove unnecessary bin files

--- ex3/list.erl
+++
@@ -1,5 +0,0 @@
--module(list).
--export([last/1]).
--spec last([T,...]) -> T.
-last([Element]) -> Element;
-last([_|Rest]) -> last(Rest).
--- ex3/test.hs
+++
@@ -1,5 +0,0 @@
-import Prelude
-
-addd' a b = addd'' (2*a) b
-    where
-        addd'' a b = a + b
