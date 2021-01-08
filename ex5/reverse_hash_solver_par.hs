a09c78badb98277694650227145e80890cba1d66
Kostis Andriopoulos <kandrio@arrikto.com>
2021-01-08 17:06:16 +0200

    Force evaluation of each computation

--- ex5/reverse_hash_solver_par.hs
+++ ex5/reverse_hash_solver_par.hs
@@ -10,13 +10,14 @@ import Data.Maybe
 import qualified Data.Set as S
 import Text.Printf (printf)
 
+force_eval a = a `seq` a
+
 -- Compute list of (hash_output, hash_input) tuples for range [curr, end] of inputs.
 hash_tuple :: (Int -> Int) -> Int -> (Int, Int)
 hash_tuple hash input = (hash input, input)
 
 hashes_of_range :: (Int -> Int) -> (Int, Int) -> [(Int, Int)]
 hashes_of_range hash (curr, end) = map (hash_tuple hash) [curr..end]
--------------------------------------------------------------------------------------------
 
 -- Create a list of 'n' (start, end) tuples that represent ranges of input values
 -- for the hash function.
@@ -26,10 +27,10 @@ ranges n counter start end =
         in 
             ((start, end'):(ranges n (counter-1) (end' + 1) end)) 
 
-parMap' :: (a -> b) -> [a] -> Eval [b]
+parMap' :: NFData b => (a -> b) -> [a] -> Eval [b]
 parMap' f [] = return []
 parMap' f (a:as) = do
-    b <- rpar (f a)
+    b <- rpar (force_eval (f a))
     bs <- parMap' f as
     return (b : bs)
 
@@ -39,5 +40,5 @@ solver_par hash _ signal box schedulers = do
     output <- runEvalIO (parMap' (hashes_of_range hash) (ranges num num 0 (round(2**27-1))))
     let flat_output = concat output
     _ <- readMVar signal
-    _ <- putMVar box (deepseq flat_output flat_output)
+    _ <- putMVar box (flat_output)
     return ()
