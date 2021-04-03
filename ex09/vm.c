ec07aaea079341e5ea8ed8d9d2fe8c844e618b97
Kostis Andriopoulos <kandrio@arrikto.com>
2021-04-03 22:06:09 +0300

    ex09: Fix segmentation fault in read_program()

--- ex09/vm.c
+++ ex09/vm.c
@@ -7,16 +7,15 @@
 int read_program(FILE *fp, uint8_t program[65536][5]) {
     int n;
     uint8_t op;
-    uint8_t byte_args[3];
+    uint8_t byte_args[4];
     int pc = 0;
-    while(fread(&op, 1, 1, fp) > 0) {
+    while (fread(&op, 1, 1, fp) > 0) {
         
         // Pass the op-code in the first element of each line
         // of the program array.
-        printf("PC: %d\n", pc);
         program[pc][0] = op;
         printf("%02X\n", op);
-
+        
         // Fill the next elements of that program line with the integer
         // parameters that may exist.
         switch(op) {
@@ -24,38 +23,66 @@ int read_program(FILE *fp, uint8_t program[65536][5]) {
                 break;
             case JUMP:
                 n = fread(byte_args, 1, 2, fp);
+                if (n!=2){
+                    printf("Error in JUMP!\n");
+                    return 1;
+                }
                 program[pc][1] = byte_args[0];
                 program[pc][2] = byte_args[1];
                 break;
             case JNZ:
-                fread(byte_args, 1, 2, fp);
+                n = fread(byte_args, 1, 2, fp);
+                if (n!=2){
+                    printf("Error in JNZ!\n");
+                    return 1;
+                }
                 program[pc][1] = byte_args[0];
                 program[pc][2] = byte_args[1];
                 break;
             case DUP:
-                fread(byte_args, 1, 1, fp);
+                n = fread(byte_args, 1, 1, fp);
+                if (n!=1){
+                    printf("Error in DUP!\n");
+                    return 1;
+                }
                 program[pc][1] = byte_args[0];
                 break;
             case SWAP:
                 fread(byte_args, 1, 1, fp);
+                if (n!=1){
+                    printf("Error in SWAP!\n");
+                    return 1;
+                }
                 program[pc][1] = byte_args[0];
                 break;
             case DROP:
                 break;
             case PUSH4:
-                fread(byte_args, 1, 4, fp);
+                n = fread(byte_args, 1, 4, fp);
+                if (n!=4){
+                    printf("Error in PUSH4!\n");
+                    return 1;
+                }
                 program[pc][1] = byte_args[0];
                 program[pc][2] = byte_args[1];
                 program[pc][3] = byte_args[2];
                 program[pc][4] = byte_args[3];
                 break;
             case PUSH2:
-                fread(byte_args, 1, 2, fp);
+                n = fread(byte_args, 1, 2, fp);
+                if (n!=2){
+                    printf("Error in PUSH2!\n");
+                    return 1;
+                }
                 program[pc][1] = byte_args[0];
                 program[pc][2] = byte_args[1];
                 break;
             case PUSH1:
-                fread(byte_args, 1, 1, fp);
+                n = fread(byte_args, 1, 1, fp);
+                if (n!=1){
+                    printf("Error in PUSH1!\n");
+                    return 1;
+                }
                 program[pc][1] = byte_args[0];
                 break;
             case ADD:                
@@ -100,14 +127,18 @@ int read_program(FILE *fp, uint8_t program[65536][5]) {
                 break;
             default:
                 printf("Error! Operator is not correct!");
+                return 0;
         }
         pc++;    
     }
+    return 0;
 }
 
+
+// These functions are meant for the manipulation of the stack.
 int pop(int stack[], int *sc) {
-    if (sc == -1) {
-        printf("Cannot pop from empty stack!\n");
+    if (*sc == -1) {
+        printf("Cannot* pop from empty stack!\n");
         return -1;
     }
     return stack[*sc--];
@@ -132,14 +163,16 @@ int swap(int *x, int *y){
 int run_program(uint8_t program[65536][5]){
     int pc = 0;
     int stack[STACK_SIZE];
-    int temp, stack_counter = -1, stack_index, stack_element;
+    int stack_counter = -1, stack_index, stack_element;
     
     while(1) {
         switch (program[pc][0]) {
             case HALT:
+                printf("HALT: Terminating program execution!\n");
                 return 0;
             case JUMP:
                 pc = program[pc][2]*8 + program[pc][1];
+                printf("JUMP: Going at PC: %d\n", pc);
                 break;
             case JNZ:
                 if(pop(stack, &stack_counter) != 0){
@@ -149,19 +182,56 @@ int run_program(uint8_t program[65536][5]){
             case DUP:
                 stack_index = stack_counter - program[pc][1];
                 stack_element = stack[stack_index];
-                push(stack, stack_counter, stack_element);
+                push(stack, &stack_counter, stack_element);
                 pc++;
                 break;
             case SWAP:
                 stack_index = stack_counter - program[pc][1];
                 swap(&stack[stack_index], &stack[stack_counter]);
                 break;
+            case DROP:
+                pop(stack, &stack_counter);
+                break;
             default:
                 printf("Something went wrong when running the program!\n");
         }
     }
 }
+/*
+// Test run_program()
+int main() {
+    uint8_t program[65536][5];
+    program[0][0] = JNZ;
+    program[0][1] = 1;
+    program[0][2] = 0;
+    program[1][0] = HALT;
+    run_program(program);
+    return 0;
+}
+*/
 
+int main(int argc, char **argv) {
+    uint8_t program[65536][5];
+    for (int i=0; i<65536; i++){
+        for (int j=0; j<5; j++){
+            program[i][j] = 0;
+        }
+    }
+    
+    FILE *fp;
+    fp = fopen(argv[1], "rb");
+    if (!fp) {
+        printf("Error opening file!\n");
+        return 1;
+    }
+
+    read_program(fp, program);
+    printf("Got out successfully!\n");
+
+    return 0;
+}
+
+/*
 int main(int argc, char **argv) {
     // We store the program commands in this array.
     // Maximum number of commands is 65536.
@@ -182,3 +252,4 @@ int main(int argc, char **argv) {
 
     return 0;
 }
+*/
